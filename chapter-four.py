0b4c3a0b2c9b9bfcb042a863f2797a024a55d40a
Piyush Sarkar <sarkarpiyush2004@gmail.com>
2026-06-04 10:53:01 +0530

    Initial commit

---
+++ chapter-four.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Chai Sales Dashboard")
+
+file = st.file_uploader("Upload your csv file", type=["csv","xlsx"])
+
+if file:
+    # Check file extension
+    if file.name.endswith(".csv"):
+        df = pd.read_csv(file)
+    elif file.name.endswith(".xlsx"):
+        df = pd.read_excel(file)
+    st.subheader("Data Preview")
+    st.dataframe(df)
+
+if file:
+    st.subheader("Summary Stats")
+    st.write(df.describe())
+
+if file:
+    cities = df["City"].unique()
+    selected_city = st.selectbox("Filter by cities", cities)
+    filtered_data = df[df["City"] == selected_city]
+    st.dataframe(filtered_data)
---
+++ chapter-one.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Hello Chai App")
+st.subheader("Brewed with streamlit")
+st.text("Welcome to your first interactive app")
+st.write("Choose your fav. variety of chai")
+
+chai = st.selectbox("Your fav chai: ", ["Masala Chai", "Lemon Tea", "Adrak Chai", "Kesar Chai"])
+st.write(f"You choose {chai}, Execellent choice")
+
+st.success("Your chai has been brewed")
